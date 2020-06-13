dcebe3dba40b5a76d8f309252ac6c448ef585c4d
fengbin <2452369568@qq.com>
2020-06-13 23:53:31 +0800

    爬虫案列

--- spilder.py
+++ spilder.py
@@ -1,2 +1,16 @@
+#pip install requests
+import requests
+
+url='https://www.runoob.com/python/python-mysql.html'
+html=requests.get(url)
+linkUrl=html.url;
+print(linkUrl)
+content=html.content;
+print(content)
+cookies=html.cookies
+print(cookies)
+headers=html.headers
+print(headers)
+#print(html.text)
 
 
